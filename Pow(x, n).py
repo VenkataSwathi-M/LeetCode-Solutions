9a217241f53ff1bbe1b8a5b15ff073a64e36ce42
VenkataSwathi-M <swathi22092004@gmail.com>
2025-07-11 21:22:04 +0530

    Pow(x, n).py

---
+++ Pow(x, n).py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        x1 = x**n
+        return x1
+solu = Solution()
+print(solu.myPow(2.10000,3))
